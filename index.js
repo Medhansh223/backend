ca350478e216b7f44b4d37adb41bb55bf20684e7
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-11-27 22:19:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB:', err));
 
