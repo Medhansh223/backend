196ddeb298f65f9395f024e498fa575674f94e91
MEDHANSH JAIN <medhanshjainfor@gmail.com>
2024-11-27 21:44:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Import routes
+const authRoutes = require('./routes/auth');
+
+// Initialize dotenv to read .env variables
+dotenv.config();
+
+const app = express();
+
+// CORS Configuration
+// const corsOptions = {
+//   origin: ['https://explore-bharat-frontend.vercel.app'], // Add your frontend URL here
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+// };
+app.use(cors({
+  origin:"https://explore-bharat-frontend.vercel.app",
+  credentials:"true",
+}));
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Could not connect to MongoDB:', err));
+
+// Routes middleware
+app.use('/api/auth', authRoutes);
+
+// Start the server
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/',(req,res)=>{
+  return res.json({
+    message:"Server UP"
+  })
+})
